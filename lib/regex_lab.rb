7364a7d5b2d075890d9f20648dd45f49c74cdbed
noven temer <noven5823@gmail.com>
2017-01-26 19:03:04 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+word.match(/^[aEiAuIo]/)?  true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+# text.match(/^[a-zA-Z]\w[\.!?]$/) ? true : false
+text.match(/^[A-Z].*[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+phone.match(/[0-9]{3}\s[0-9]{3}\s[0-9]{4}\Z|\([0-9]{3}\)[0-9]{3}-[0-9]{4}$|\([0-9]{3}\)[0-9]{3}[0-9]{4}$|[0-9]{10}/) ? true : false
 end
